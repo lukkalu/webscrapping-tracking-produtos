549226d3ff7574275b17058a2db8e42cbbf1eb40
Lucas Heluany de Souza <57162716+lukkalu@users.noreply.github.com>
2022-01-06 13:24:18 -0300

    Update webscrapping-produtos.py

--- webscrapping-produtos.py
+++ webscrapping-produtos.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # importando as bibliotecas
 
 import sqlite3
@@ -24,8 +21,10 @@ data_atual_formatada = data_atual.strftime('%d/%m/%Y %H:%M:%S')
 
 # definir os produtos para consulta e a variavel de append
 
-produtos_consulta = ['iphone 12', 'BRE57AK', 'logitech g29', 'nintendo switch']
+produtos_consulta = ['iphone 12', 'BRE57AK', 'BRM54HK', 'logitech g29', 'WD11M4453JX', 'AOC 27 Hero 144HZ', 'CD060AE', 'RYZEN 5 5600x' , 'asus prime a520m']
+#produtos_consulta = ['AOC 27 Hero 144HZ']
 lista_dados_produto = []
+lista_aval = []
 produto_consultado = []
 
 ## Instanciando o navegador e pegando os dados via xPath
@@ -39,23 +38,34 @@ for p in produtos_consulta:
     
     bloco_valores = navegador.find_elements_by_xpath("//*[starts-with(@class, 'mnr-c pla-unit')]")
     bloco_links = navegador.find_elements_by_xpath("//*[starts-with(@class,'plantl tkXAec')]")
+    bloco_aval = navegador.find_elements_by_xpath("//*[starts-with(@class,'pla-extensions-container')]")
     
     # preparando a lista com os dados que vamos utilizar
     i = 0
     while i < len(bloco_valores):
         lista_dados_produto.append(bloco_valores[i].text + '\n' + bloco_links[i].get_attribute("href") + '\n' +  p)
+        lista_aval.append(bloco_aval[i].text)
         #print(bloco_valores[i].text + '\n' + links[i])
         i += 1
         
+    cont= 0
+    lista_corrigida = []
+    while cont < len(lista_dados_produto):
+        lista_corrigida.append(lista_dados_produto[cont].replace(lista_aval[cont],'').replace('\n\n','\n'))
+        #print(lista_dados_produto[cont])
+        #print(lista_aval[cont])
+        cont = cont + 1
+            
 # Criando a series para inserir no Data Frame
 
-series_dados_produto = pd.Series(data = lista_dados_produto).str.replace('PROMOÇÃO\n','', regex = True).str.replace('Retirar amanhã\n','',regex = True).str.replace('Retirar hoje\n','',regex = True).str.replace('Na loja\n','', regex = True).str.replace('REDUÇÃO NO PREÇO\n', '', regex = True).str.replace('(','', regex = True).str.replace(')','', regex = True).str.replace('+','', regex = True).str.replace('4k\n','', regex = True).str.replace('3k\n','', regex = True).str.replace('2k\n','', regex = True).str.replace('1k\n','', regex = True).str.replace('5k\n','', regex = True).str.replace('6k\n','', regex = True).str.replace("'", '', regex = True).str.split('\n', expand=True)
+series_dados_produto = pd.Series(data = lista_corrigida).str.replace('PROMOÇÃO\n','', regex = True).str.replace('Retirar amanhã\n','',regex = True).str.replace('Retirar hoje\n','',regex = True).str.replace('Na loja\n','', regex = True).str.replace('REDUÇÃO NO PREÇO\n', '', regex = True).str.replace('(','', regex = True).str.replace(')','', regex = True).str.replace('+','', regex = True).str.replace('4k\n','', regex = True).str.replace('3k\n','', regex = True).str.replace('2k\n','', regex = True).str.replace('1k\n','', regex = True).str.replace('5k\n','', regex = True).str.replace('6k\n','', regex = True).str.replace("'", '', regex = True).str.split('\n', expand=True)
 
 #criação e tratamento do DF
 
 dados_produto_df = pd.DataFrame(series_dados_produto).rename(columns = {0:'nome_produto', 1:'valor_produto', 2:'loja', 3:'link_compra', 4:'produto'}, inplace = False)
 dados_produto_df = dados_produto_df[['loja', 'nome_produto', 'valor_produto', 'link_compra', 'produto']]
-dados_produto_df['valor_produto'] = pd.to_numeric(dados_produto_df['valor_produto'].str[3:11].str.replace('.','', regex = True).str.replace(',' , '.' ,regex = True))
+#dados_produto_df['valor_produto'] = pd.to_numeric(dados_produto_df['valor_produto'].str[3:11].str.replace('.','', regex = True).str.replace(',' , '.' ,regex = True))
+dados_produto_df['valor_produto'] = pd.to_numeric(dados_produto_df['valor_produto'].str[3:].str.split(',').str[0].str.replace('.','',regex = True) + '.' + dados_produto_df['valor_produto'].str.split(',').str[1].str[:2])
 dados_produto_df['data_consulta'] = data_atual_formatada
 dados_produto_df
 
@@ -70,7 +80,8 @@ dados_produto_df.to_csv(caminho,index=False)
 
 #### instanciando conexão com o banco ####
 
-cnx = mysql.connector.connect(user='seu usuario',password='sua senha', host='seu host', database='seu banco')
+cnx = mysql.connector.connect(user='root',password='Colg@te15', host='35.239.95.164', database='scrapping_produtos')
+#cnx = mysql.connector.connect(user='seu usuario',password='sua senha', host='seu host', database='seu banco')
 
 cursor = cnx.cursor()
 
@@ -100,4 +111,3 @@ for i in range(len(dados_produto_df)):
 #fechar a conexão
 
 cnx.close()
-
